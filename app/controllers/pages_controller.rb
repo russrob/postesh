41cfecf3a099cf0746d767e28447d0b12a4fae71
Russell Roberts <russrob23@gmail.com>
2016-09-14 09:22:00 -0700

    Added all the pages for the front page

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+
+  def home
+  end
+
+  def feature 
+  end 
+
+  def pricing
+  end 
+
+  def about
+  end 
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'feature' => 'pages#feature'
+  get 'pricing' => 'pages#pricing'
+  get 'about' => 'pages#about'
+  get 'pages/home'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'pages#home'
 end
